06726dee894fb09476bdef06eca979f3f0b9ca61
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-07-03 16:46:29 +0200

    fix: exclude pino from bundled

--- packages/liteparse-grpc/buildscripts/build-server.cjs
+++ packages/liteparse-grpc/buildscripts/build-server.cjs
@@ -12,6 +12,11 @@ esbuild
     target: "node24",
     format: "esm",
     sourcemap: true,
-    external: ["@llamaindex/liteparse", "@grpc/grpc-js", "@bufbuild/protobuf"],
+    external: [
+      "@llamaindex/liteparse",
+      "@grpc/grpc-js",
+      "@bufbuild/protobuf",
+      "pino",
+    ],
   })
   .catch(() => process.exit(1));
